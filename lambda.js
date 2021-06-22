9bac430c119d0ee92cb63ebb706ccbdccb5510c4
Yorman Urdaneta <68403460+Atenadru@users.noreply.github.com>
2021-06-22 17:42:30 -0500

    Delete lambda.js

--- lambda.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./api/app')
-const server = awsServerlessExpress.createServer(app)
-module.exports.handler = (event, context) =>
-  awsServerlessExpress.proxy(server, event, context)
